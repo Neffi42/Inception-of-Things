84468b94d2e4c25c7b3782078d8bf8e163e7041f
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-11-07 16:06:46 +0100

    p2 in progress

---
+++ p2/scripts/init_server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# apt-get update
+# apt-get install -y curl
+
+# curl -sfL https://get.k3s.io | sh -
+
+# kubectl create deploy app-one --image=paulbouwer/hello-kubernetes:1.10
+# kubectl expose deploy app-one --port=80
+
+# kubectl create deploy app-two --image=paulbouwer/hello-kubernetes:1.10 --replicas=3
+# kubectl expose deploy app-two --port=80
+
+# kubectl create deploy app-three --image=paulbouwer/hello-kubernetes:1.10
+# kubectl expose deploy app-three --port=80
