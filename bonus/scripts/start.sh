ab1b416bb392fc3c00ffefa7ca01f12ac8e59077
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-11-08 16:15:01 +0100

    copy p3 files in bonus directory

---
+++ bonus/scripts/start.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo " * Creating a k3d cluster"
+k3d cluster create default -p 443:443 -p 8888:30010
+
+echo " * Installing Argo CD"
+kubectl create ns argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -k patchs
+kubectl apply -n argocd -f confs
+
+echo " * Setting 'argocd' as default namespace"
+kubectl config set-context --current --namespace=argocd
+
+echo " * Waiting for argocd to come online"
+sleep 160
+
+echo " * Configuring Argo CD to communicate with k3d"
+argocd login --core
+
+echo " * Installing Will's app"
+kubectl create ns dev
+argocd app create will-playground --repo https://github.com/Neffi42/iot-acroue --path . --dest-server https://kubernetes.default.svc --sync-policy automated --dest-namespace dev
+
+echo " * Setting back 'default' as default namespace"
+kubectl config set-context --current --namespace=default
+
+echo "Applications successfully deployed"
+
+echo " - Argo CD password:"
+argocd admin initial-password -n argocd
