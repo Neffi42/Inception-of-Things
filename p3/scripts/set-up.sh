8f379033c41487ea5637a1e985ff6bb547f4af28
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-11-08 15:19:49 +0100

    add installation of argocd to set-up.sh

--- p3/scripts/set-up.sh
+++ p3/scripts/set-up.sh
@@ -20,8 +20,13 @@ sudo docker --version
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
-rm kubectl 
+rm kubectl
 
 # Install k3d
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 k3d version
+
+# Install Argo CD CLI
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
